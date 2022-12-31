48899137a76b8106dd34e8ab6f4b87961cd47775
cleverAkanimoh <crushclever1@gmail.com>
2022-12-31 20:41:38 +0100

    changed 3 files

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -1,10 +1,21 @@
+import memesData from "../memeData";
+
 const MainContent = () => {
-    return ( 
+
+    const getMeme = async e => {
+        // let url = ``;
+
+        e.preventDefault()
+        let response = fetch(memesData);
+        let data = response.json();
+        console.log(data)
+    }
+    return (
         <section id="container" >
-            <form id="form-container">
+            <form id="form-container" onSubmit={getMeme}>
                 <input type='text' placeholder="enter top text" />
                 <input type='text' placeholder="enter bottom text" />
-                <button id="form-btn" >Get a new meme image</button>
+                <button id="form-btn">Get a new meme image</button>
             </form>
             <div id="meme-display"></div>
         </section>
