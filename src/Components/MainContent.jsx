8979478e7a4083df4aa3459d02193318d6cf742f
cleverAkanimoh <crushclever1@gmail.com>
2022-12-31 23:12:06 +0100

    auto-commit

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -4,25 +4,22 @@ import memesData from "../memeData";
 
 const MainContent = () => {
 
-    const [url, setUrl] = useState(memesData);
+    const [memeUrl, setMemeUrl] = useState("");
 
-    const getMeme = e => {
+    const getMemeUrl = () => {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        let url = memesArray[randomNumber].url
+        setMemeUrl(memesArray[randomNumber].url)
     }
-    const displayMeme = memesArray.map(
-        meme => <img key={id} id="meme-display" src={url} alt="meme-image" />
-    )
 
     return (
         <section id="container">
             <div id="form-container">
                 <input type='text' placeholder="enter top text" />
                 <input type='text' placeholder="enter bottom text" />
-                <button id="form-btn" onClick={getMeme}>Get a new meme image</button>
+                <button id="form-btn" onClick={getMemeUrl}>Get a new meme image</button>
             </div>
-            {displayMeme}
+            <img id="meme-display" src={memeUrl} alt="meme-image" />
         </section>
     );
 }
