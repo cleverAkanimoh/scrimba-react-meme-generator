71f4053e6ad9417373779da3d45edb22afa58292
cleverAkanimoh <crushclever1@gmail.com>
2022-12-31 21:34:42 +0100

    modified MainContent.jsx in Components

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -4,15 +4,17 @@ import memesData from "../memeData";
 
 const MainContent = () => {
 
-    const [meme, setMeme] = useState(memesData);
+    const [url, setUrl] = useState(memesData);
 
-    const getMeme = async e => {
-
-        const memesArray = meme.data.memes
+    const getMeme = e => {
+        const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         let url = memesArray[randomNumber].url
-        console.log(memesArray)
     }
+    const displayMeme = memesArray.map(
+        meme => <img key={id} id="meme-display" src={url} alt="meme-image" />
+    )
+
     return (
         <section id="container">
             <div id="form-container">
@@ -20,9 +22,9 @@ const MainContent = () => {
                 <input type='text' placeholder="enter bottom text" />
                 <button id="form-btn" onClick={getMeme}>Get a new meme image</button>
             </div>
-            <img id="meme-display" src='{url}' alt="meme-image" />
+            {displayMeme}
         </section>
-     );
+    );
 }
- 
+
 export default MainContent;
