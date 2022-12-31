9bea041370e4ddcf7ef87dcd606dd70e2ee6b088
cleverAkanimoh <crushclever1@gmail.com>
2022-12-31 09:18:09 +0100

    changed 6 files

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../reactjs-icon-2.svg';
+
+const Header = () => (
+    <header>
+        <div>
+            <img src={logo} className="logo" alt="logo" />
+            <h3>Meme Generator</h3>
+        </div>
+        <h4>React Course - Project 3</h4>
+    </header>
+    );
+
+export default Header;
---
+++ src/Components/MainContent.jsx
@@ -0,0 +1,9 @@
+const MainContent = () => {
+    return ( 
+        <article>
+            <h2>Maincontent Component</h2>
+        </article>
+     );
+}
+ 
+export default MainContent;
