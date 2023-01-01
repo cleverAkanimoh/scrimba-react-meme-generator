efabd5b046339b0bacf15cdb196c95be637ac8ab
cleverAkanimoh <crushclever1@gmail.com>
2023-01-01 11:45:45 +0100

    modified MainContent.jsx and index.css

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react';
-
 import defaultmemeImage from "../defaultMemeImage.jpg";
 import memesData from "../memeData";
 
 const MainContent = () => {
 
     const [meme, setMeme] = useState({
-        topText: "",
-        bottomText: "",
+        topText: "top text will go here",
+        bottomText: "bottom text will go here",
         randomImage: defaultmemeImage
     })
     const [allMemeImages, setAllMemeImages] = useState(memesData)
@@ -22,14 +21,26 @@ const MainContent = () => {
         }))
     }
 
+    const topTextValueUpdate = () => setMeme(prevMeme => ({
+        ...prevMeme,
+        topText: 'top text value'
+    }))
+
+    const bottomTextValueUpdate = () => setMeme(prevMeme => ({
+        ...prevMeme,
+        bottomText: 'bottom text value'
+    }))
+
     return (
         <section id="container">
             <div id="form-container">
-                <input type='text' placeholder="enter top text" />
-                <input type='text' placeholder="enter bottom text" />
+                <input type='text' onInput={topTextValueUpdate} placeholder="enter top text" />
+                <input type='text' onInput={bottomTextValueUpdate} placeholder="enter bottom text" />
                 <button id="form-btn" onClick={getMemeUrl}>Get a new meme image</button>
             </div>
             <picture>
+                <p id="topText">{ meme.topText }</p>
+                <p id="bottomText">{ meme.bottomText }</p>
                 <img id="meme-display" src={meme.randomImage} alt="meme-image" />
             </picture>
         </section>
